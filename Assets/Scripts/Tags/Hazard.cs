197f3c246480ce1fb7139b477ae3533a3a3bdecb
Natalia DaLomba <nataliadalomba@protonmail.com>
2023-02-12 21:41:03 -0500

    Carlos: Hazard rotation started.I would like hazards to start at a random rotation on all axes and to rotate in all axes

--- Assets/Scripts/Tags/Hazard.cs
+++ Assets/Scripts/Tags/Hazard.cs
@@ -5,35 +5,41 @@ public class Hazard : MonoBehaviour {
     [SerializeField] private float verticalSpeed;
     [SerializeField] private float amplitude;
 
+    [SerializeField] private float rotSpeed;
+
     private Vector3 origPos;
     private Vector3 tempPos;
 
-    private Quaternion origRot;
-    private Quaternion tempRot;
+    private Vector3 rot = new Vector3(45f, 45f, 45f);
 
-    private static System.Random rnd = new System.Random();
+    private float rotDegreesPerSecond = 45f;
+
+    private static System.Random rndFloat = new System.Random();
+    private static System.Random rndRot = new System.Random();
 
     private void Start () {
-        tempPos = origPos = new Vector3(transform.position.x, (float) rnd.NextDouble() + .3f, transform.position.z);
-        verticalSpeed = (float)rnd.NextDouble();
+        tempPos = origPos = new Vector3(transform.position.x, (float) rndFloat.NextDouble() + .3f, transform.position.z);
+        verticalSpeed = (float)rndFloat.NextDouble();
 
-        tempRot = origRot = transform.rotation;
+        //rot = Quaternion.Euler((float)rndRot.NextDouble(), (float)rndRot.NextDouble(), (float)rndRot.NextDouble());
+
     }
 
     private void FixedUpdate() {
         RandomFloating();
-        tempRot = origRot = transform.rotation;
-
-        float elapsedTime = 0;
-
-        transform.rotation = Quaternion.Lerp(origRot, tempRot, (elapsedTime));
-        elapsedTime += Time.deltaTime;
+        Rotation();
     }
 
     private void RandomFloating() {
         tempPos = origPos;
         tempPos.y += Mathf.Sin(Time.realtimeSinceStartup * (verticalSpeed + 2)) * amplitude;
         transform.position = tempPos;
+    }
 
+    private void Rotation() {
+        //float yAngle = transform.rotation.eulerAngles.y;
+
+        transform.rotation = Quaternion.Euler(rot * Time.deltaTime) * transform.rotation;
+        //transform.rotation = Quaternion.AngleAxis(yAngle + (Time.deltaTime * rotDegreesPerSecond), Vector3.up);
     }
 }
